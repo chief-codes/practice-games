0486ae0f1faf807de3489d08ff1435c0e2d54ec8
chief-codes <me.chief@proton.me>
2022-08-21 08:45:23 +0000

    Add files via upload

---
+++ Madlib2.py
@@ -0,0 +1,25 @@
+input("\nThis is a madlib game about Activities done within the day. \n\
+    The activities should be in order from the wake up time to the time to sleep.\n GOOD LUCK!!!! ")
+
+activity1 = input("Enter Activity 1: \n")
+activity2 = input("Enter Activity 2: \n")
+activity3 = input("Enter Activity 3: \n")
+activity4 = input("Enter Activity 4: \n")
+activity5 = input("Enter Activity 5: \n")
+activity6 = input("Enter Activity 6: \n")
+activity7 = input("Enter Activity 7: \n")
+activity8 = input("Enter Activity 8: \n")
+activity9 = input("Enter Activity 9: \n")
+activity10 = input("Enter Activity 10: \n")
+
+Madlib = f" Every morning when I wake up I first {activity1}. Then I {activity2} my face, \
+and {activity3} my teeth. Since I'm a fitness conscious person, I {activity4} for \
+30 minutes to an hour every morning right after brushing my teeth.\n\
+    The next thing I do is {activity5}, don't forget, 'Cleanliness is next to Godliness!!!'. \
+After that I'll {activity6} my breakfast. It is a good practice to read {activity7} everyday \
+so I like {activity8} everyday to keep my brain sharp and knowledge high.\n     At this point \
+I do some {activity9} with friends and hangout for a while. I {activity10} once again in the evening \
+and then go to bed" 
+
+
+print(Madlib)
